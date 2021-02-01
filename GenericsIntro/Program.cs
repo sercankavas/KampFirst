accd4940056866e0e89581e283722d681bd5a4f8
Sercan <srsr141986@gmail.com>
2021-02-01 13:11:14 +0100

    last

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -7,9 +7,17 @@ namespace GenericsIntro
         static void Main(string[] args)
         {
             MyList<string> names = new MyList<string>();
-            names.Add("Sercan");
+            names.Add("Sercan");//->kendi Add fonksiyonum
 
+            Console.WriteLine(names.Length);//--> benim kendi lengt foksiyonum bu
 
+            names.Add("Sinan");
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
