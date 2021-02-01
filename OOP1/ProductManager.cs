42b20c5af98d3f305c71135bc8379707e71a8d2f
Sercan <srsr141986@gmail.com>
2021-02-01 12:03:38 +0100

    Collections added

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager //ProductManager olan basliklarda urun ile alakali operasyonlar var demektir
+    {
+        public void Add(Product product)//-->encapsulation bu. parametre olarak bir urun cinsinden verdik,yani tek tek ad,fiyat, id
+                    // filan demek yerine urunu tum ozellikleriyle iceren product classi cinsinden bi parameter veriyoruz. 
+        {
+            Console.WriteLine(product.ProductName + " added.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " updated.");
+
+        }
+
+        public int Topla(int sayi1, int sayi2) // bana topla fonksiyonunun degeri lazim ve 
+            //ben bunu biyerde kullanacaksam void yerine tur deger kullanirim
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)  //voidi emir kipiyle yapilacak islerde kullanirim. methodun degerini kullanamam
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.Category = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStok = 3;
+
+            // ya da bunu bu sekilde de olusturabiliriz
+
+            Product product2 = new Product { Id = 2, Category = 5, UnitsInStok = 5, 
+                ProductName = "Kalem", UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            
+
+
+        }
+    }
+}
